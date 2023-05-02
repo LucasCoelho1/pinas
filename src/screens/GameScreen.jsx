47653154f6c3fbecca2e089db36a4416d2905c92
LucasCoelho1 <lucas.coelho3@alunos.sc.senac.br>
2023-05-02 14:12:41 -0300

    nada

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -1,7 +1,171 @@
 import React, { useState, useEffect } from "react";
 import { View } from "react-native";
+import { Text, Image, Button } from "react-native-paper";
 
 export default function GameQuiz() {
+
+  const mock = [
+    {
+      id: 1,
+      Name: "Gekko",
+      Image:
+        "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 2,
+      Name: "Fade",
+      Image:
+        "https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 3,
+      Name: "Breach",
+      Image:
+        "https://media.valorant-api.com/agents/5f8d3a7f-467b-97f3-062c-13acf203c006/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 4,
+      Name: "Raze",
+      Image:
+        "https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 5,
+      Name: "Chamber",
+      Image:
+        "https://media.valorant-api.com/agents/22697a3d-45bf-8dd7-4fec-84a9e28c69d7/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 6,
+      Name: "KAY/O",
+      Image:
+        "https://media.valorant-api.com/agents/601dbbe7-43ce-be57-2a40-4abd24953621/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 7,
+      Name: "Skye",
+      Image:
+        "https://media.valorant-api.com/agents/6f2a04ca-43e0-be17-7f36-b3908627744d/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 8,
+      Name: "Cypher",
+      Image:
+        "https://media.valorant-api.com/agents/117ed9e3-49f3-6512-3ccf-0cada7e3823b/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 9,
+      Name: "Sova",
+      Image:
+        "https://media.valorant-api.com/agents/ded3520f-4264-bfed-162d-b080e2abccf9/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 10,
+      Name: "Killjoy",
+      Image:
+        "https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 11,
+      Name: "Harbor",
+      Image:
+        "https://media.valorant-api.com/agents/95b78ed7-4637-86d9-7e41-71ba8c293152/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 12,
+      Name: "Viper",
+      Image:
+        "https://media.valorant-api.com/agents/707eab51-4836-f488-046a-cda6bf494859/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 13,
+      Name: "Phoenix",
+      Image:
+        "https://media.valorant-api.com/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 14,
+      Name: "Astra",
+      Image:
+        "https://media.valorant-api.com/agents/41fb69c1-4189-7b37-f117-bcaf1e96f1bf/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 15,
+      Name: "Brimstone",
+      Image:
+        "https://media.valorant-api.com/agents/9f0d8ba9-4140-b941-57d3-a7ad57c6b417/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 16,
+      Name: "Neon",
+      Image:
+        "https://media.valorant-api.com/agents/bb2a4828-46eb-8cd1-e765-15848195d751/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 17,
+      Name: "Yoru",
+      Image:
+        "https://media.valorant-api.com/agents/7f94d92c-4234-0a36-9646-3a87eb8b5c89/displayicon.png",
+      respCorrect: 1,
+    },
+    {
+      id: 18,
+      Name: "Sage",
+      Image:
+        "https://media.valorant-api.com/agents/569fdd95-4d10-43ab-ca70-79becc718b46/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 19,
+      Name: "Reyna",
+      Image:
+        "https://media.valorant-api.com/agents/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc/displayicon.png",
+      respCorrect: 3,
+    },
+    {
+      id: 20,
+      Name: "Omen",
+      Image:
+        "https://media.valorant-api.com/agents/8e253930-4c05-31dd-1b6c-968525494517/displayicon.png",
+      respCorrect: 2,
+    },
+    {
+      id: 21,
+      Name: "Jett",
+      Image:
+        "https://media.valorant-api.com/agents/add6443a-41bd-e414-f6ad-e58d267f4e95/displayicon.png",
+      respCorrect: 2,
+    },
+  ];
+  
+
+  const returnRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random() * 21) + 1;
+
+    if (randomNumber === 0) {
+        return 1;
+    } else {
+        return randomNumber;
+    }
+}
+
+
   return (
     <View>
       <Text>GameQuiz</Text>
@@ -9,158 +173,8 @@ export default function GameQuiz() {
       <Button>opção 1</Button>
       <Button>opção 2</Button>
       <Button>opção 3</Button>
-    </View>
-  );
+    </View> 
+  ); 
+
 }
 
-const mock = [
-  {
-    id: 1,
-    Name: "Gekko",
-    Image:
-      "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 2,
-    Name: "Fade",
-    Image:
-      "https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 3,
-    Name: "Breach",
-    Image:
-      "https://media.valorant-api.com/agents/5f8d3a7f-467b-97f3-062c-13acf203c006/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 4,
-    Name: "Raze",
-    Image:
-      "https://media.valorant-api.com/agents/f94c3b30-42be-e959-889c-5aa313dba261/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 5,
-    Name: "Chamber",
-    Image:
-      "https://media.valorant-api.com/agents/22697a3d-45bf-8dd7-4fec-84a9e28c69d7/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 6,
-    Name: "KAY/O",
-    Image:
-      "https://media.valorant-api.com/agents/601dbbe7-43ce-be57-2a40-4abd24953621/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 7,
-    Name: "Skye",
-    Image:
-      "https://media.valorant-api.com/agents/6f2a04ca-43e0-be17-7f36-b3908627744d/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 8,
-    Name: "Cypher",
-    Image:
-      "https://media.valorant-api.com/agents/117ed9e3-49f3-6512-3ccf-0cada7e3823b/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 9,
-    Name: "Sova",
-    Image:
-      "https://media.valorant-api.com/agents/ded3520f-4264-bfed-162d-b080e2abccf9/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 10,
-    Name: "Killjoy",
-    Image:
-      "https://media.valorant-api.com/agents/1e58de9c-4950-5125-93e9-a0aee9f98746/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 11,
-    Name: "Harbor",
-    Image:
-      "https://media.valorant-api.com/agents/95b78ed7-4637-86d9-7e41-71ba8c293152/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 12,
-    Name: "Viper",
-    Image:
-      "https://media.valorant-api.com/agents/707eab51-4836-f488-046a-cda6bf494859/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 13,
-    Name: "Phoenix",
-    Image:
-      "https://media.valorant-api.com/agents/eb93336a-449b-9c1b-0a54-a891f7921d69/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 14,
-    Name: "Astra",
-    Image:
-      "https://media.valorant-api.com/agents/41fb69c1-4189-7b37-f117-bcaf1e96f1bf/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 15,
-    Name: "Brimstone",
-    Image:
-      "https://media.valorant-api.com/agents/9f0d8ba9-4140-b941-57d3-a7ad57c6b417/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 16,
-    Name: "Neon",
-    Image:
-      "https://media.valorant-api.com/agents/bb2a4828-46eb-8cd1-e765-15848195d751/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 17,
-    Name: "Yoru",
-    Image:
-      "https://media.valorant-api.com/agents/7f94d92c-4234-0a36-9646-3a87eb8b5c89/displayicon.png",
-    respCorrect: 1,
-  },
-  {
-    id: 18,
-    Name: "Sage",
-    Image:
-      "https://media.valorant-api.com/agents/569fdd95-4d10-43ab-ca70-79becc718b46/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 19,
-    Name: "Reyna",
-    Image:
-      "https://media.valorant-api.com/agents/a3bfb853-43b2-7238-a4f1-ad90e9e46bcc/displayicon.png",
-    respCorrect: 3,
-  },
-  {
-    id: 20,
-    Name: "Omen",
-    Image:
-      "https://media.valorant-api.com/agents/8e253930-4c05-31dd-1b6c-968525494517/displayicon.png",
-    respCorrect: 2,
-  },
-  {
-    id: 21,
-    Name: "Jett",
-    Image:
-      "https://media.valorant-api.com/agents/add6443a-41bd-e414-f6ad-e58d267f4e95/displayicon.png",
-    respCorrect: 2,
-  },
-];
-
-return <View style={styles.container}></View>;
